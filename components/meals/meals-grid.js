87acaf447dd9317b7832ea374238667e66dcd917
karim <karimalaa298@gmail.com>
2026-04-16 10:55:27 +0300

    Add meals page and component and fetch from the backend using sql lite db

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -3,10 +3,11 @@ import classes from './meals-grid.module.css';
 
 export default function MealsGrid({meals}){
     return(
-        <ul>
-            {meals.map(meal => <li key={meal.id}>
+        <ul className={classes.meals}>
+            {meals.map(meal => (
+            <li key={meal.id}>
                 <MealItem {...meal} />
-            </li>)}
+            </li>))}
         </ul>
     );
 }
