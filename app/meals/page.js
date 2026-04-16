40d548389b87672db59e30d819b19ca680cfac33
karim <karimalaa298@gmail.com>
2026-04-16 09:39:48 +0300

    setup meal page with sqllite database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,26 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+
 export default function MealsPage() {
     return(
-        <h1>Meals Page</h1>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious Meals, created <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                        Share your Faviorite Recipe
+                    </Link>                    
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]}  />
+            </main>
+        </>
     );
 }
